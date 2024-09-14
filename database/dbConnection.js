c11ac6f3c7b9e9b8d77c5eaf8b5f1604fb568f07
AL-HussienAbdAlrazeq <hessenabdalrazek1@gmail.com>
2024-09-14 17:16:26 +0300

    E-Commerce

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-
-
-export const dbConnection = mongoose.connect(process.env.DB_ONLINE_CONNECTION).
-then(()=>{
-    console.log("The DataBase Connection Was Successfully")
-})
+export const dbConnection = mongoose
+  .connect(process.env.DB_ONLINE_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+  })
+  .then(() => {
+    console.log("The DataBase Connection Was Successfully");
+  });
